4ce93523a5667a2b70ee442cb6c26a1f52639a64
Zackyization <zackyization@gmail.com>
2021-10-31 01:32:44 +0800

    Add experimental js, some TODO notes.

---
+++ youtube-productivity/youtube-productivity.js
@@ -0,0 +1,5 @@
+let ytBrowse = document.getElementsByTagName("ytd-browse")[0];
+ytBrowse.parentNode.removeChild(ytBrowse);
+
+// TODO: Remove ytBrowse area, insert HTML showing text like "Let's get started" or smth that motivates the user to be productive.
+// TODO: Extension logo, make in inkscape
