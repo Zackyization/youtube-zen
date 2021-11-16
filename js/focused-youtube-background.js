f3fc96dd0040c72420c902e6f77a7296ea36e8ff
Zackyization <zackyization@gmail.com>
2021-11-16 10:42:23 +0800

    Comitting before booking in

--- js/focused-youtube-background.js
+++ js/focused-youtube-background.js
@@ -1,7 +1,6 @@
 const YOUTUBE_DOMAIN = new RegExp("*://*.youtube.com/*");
 const YOUTUBE_HOME_STYLE = "style/youtube-home.css";
 const YOUTUBE_VIDEO_STYLE = "style/youtube-video.css";
-
 /*
 NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API for reference
 NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/insertCSS, more reference
@@ -33,6 +32,15 @@ function isItYoutube(tab) {
     return YOUTUBE_DOMAIN.test(tab.url);
 }
 
+function handleMessage(request, sender, sendResponse) {
+    console.log("Message from the content script: " +
+        request.greeting);
+    sendResponse({
+        response: "Response from background script"
+    });
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
 
 /*
 When first loaded, initialize the extension
@@ -54,7 +62,29 @@ gettingAllTabs.then((tabs) => {
 /*
 Each time a tab is updated, reinitialize the page action for that tab.
 */
+
+
 //When a tab is updated, execute the "focus" script
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
     initializePageAction(tab);
+});
+
+browser.browserAction.onClicked.addListener(() => {
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    let querying = browser.tabs.query({
+        //LEFT OFF HERE, figure out how to only query the tab IDs with youtube URLs
+        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/query
+    });
+
+    let insertingCSS = browser.tabs.insertCSS(2, {
+        file: "css/youtube-home.css"
+    });
+
+
+
+    //LEFT OFF HERE, find a way to replace the '2' value with the tabID who's URL matches youtube
+    insertingCSS.then(null, onError);
 });
--- js/remove-home-elements.js
+++ js/remove-home-elements.js
@@ -1,34 +1,9 @@
 let ytBrowse = document.getElementsByTagName("ytd-browse")[0];
 ytBrowse.textContent = ''; //wipes the browse section on the home page
-function handleResponse(message) {
-    console.log(`Message from the background script:  ${message.response}`);
-}
 
-function handleError(error) {
-    console.log(`Error: ${error}`);
-}
-
-function notifyBackgroundPage(e) {
-    let sending = browser.runtime.sendMessage({
-        greeting: "Greeting from the content script"
-    });
-    sending.then(handleResponse, handleError);
-}
-
-
-
-//Remove the youtube home page area and insert an indicator that the extension is enabled.
-ytBrowse.innerHTML = `<div id="focused-youtube-area">
+ytBrowse.innerHTML = `<div id="youtube-productivity-tool-area">
     <h1>Productivity Mode enabled.</h1>
 </div>`;
-
-window.addEventListener("load",notifyBackgroundPage);
-
-
-
-
-
-
 
 /*
 TODO: Web extension popup menu that lets users toggle between productivity mode on/off.
