2a2810e0921444a636ad1754dad4dc042206f9e7
Zackyization <zackyization@gmail.com>
2021-11-21 19:16:22 +0800

    Committing to move to another machine

--- js/focused-youtube-background.js
+++ js/focused-youtube-background.js
@@ -1,90 +1,24 @@
-const YOUTUBE_DOMAIN = new RegExp("*://*.youtube.com/*");
 const YOUTUBE_HOME_STYLE = "style/youtube-home.css";
 const YOUTUBE_VIDEO_STYLE = "style/youtube-video.css";
 /*
 NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API for reference
 NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/insertCSS, more reference
-NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onUpdated, more reference
+NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts#communicating_with_background_scripts, more reference
+    https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/executeScript for executing a content script on a specific tab(s). [For youtube Tabs?]
 TODO: Add CSS only when domain name is youtube.com
 */
 
-function initializePageAction(tab) {
-    console.log("Updated tab: " + tabId);
-    console.log("Changed attributes: ");
-    console.log(changeInfo);
-    console.log("New tab Info: ");
-    console.log(tabInfo);
-
-    if (isItYoutube(tab)) {
-        //inject css
-        function onError(error) {
-            console.log(`Error: ${error}`);
-        }
-
-        let insertingCSS = browser.tabs.insertCSS(2, {
-            file: YOUTUBE_HOME_STYLE
-        });
-        insertingCSS.then(null, onError);
+function injectCSS(message) {
+    if (message === "connected") {
+        //prepare the css in the message to be sent back to the content script
     }
 }
 
-function isItYoutube(tab) {
-    return YOUTUBE_DOMAIN.test(tab.url);
+browser.runtime.onMessage.addListener(notify);
+
+function notify(message) {
+    console.log({"Message": message});
 }
 
-function handleMessage(request, sender, sendResponse) {
-    console.log("Message from the content script: " +
-        request.greeting);
-    sendResponse({
-        response: "Response from background script"
-    });
-}
-
-browser.runtime.onMessage.addListener(handleMessage);
-
-/*
-When first loaded, initialize the extension
-*/
-
-//Check all tabs
-//See which tabs has the youtube URL
-
-var gettingAllTabs = browser.tabs.query({});
-console.log(gettingAllTabs);
-gettingAllTabs.then((tabs) => {
-    for (let tab of tabs) {
-        console.log(tab);
-        initializePageAction(tab);
-    }
-});
-
-
-/*
-Each time a tab is updated, reinitialize the page action for that tab.
-*/
-
-
-//When a tab is updated, execute the "focus" script
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-    initializePageAction(tab);
-});
-
-browser.browserAction.onClicked.addListener(() => {
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-
-    let querying = browser.tabs.query({
-        //LEFT OFF HERE, figure out how to only query the tab IDs with youtube URLs
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/query
-    });
-
-    let insertingCSS = browser.tabs.insertCSS(2, {
-        file: "css/youtube-home.css"
-    });
-
-
-
-    //LEFT OFF HERE, find a way to replace the '2' value with the tabID who's URL matches youtube
-    insertingCSS.then(null, onError);
-});
+//connect to the background script
+//NOTE: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts#connection-based_messaging, this section explains the messaging procedure
--- js/focused-youtube.js
+++ js/focused-youtube.js
@@ -1,5 +1,5 @@
-let ytBrowse = document.getElementsByTagName("ytd-browse")[0];
-ytBrowse.textContent = ''; //wipes the browse section on the home page
+const YOUTUBE_BROWSING_AREA = document.getElementsByTagName("ytd-browse")[0];
+
 function handleResponse(message) {
     console.log(`Message from the background script:  ${message.response}`);
 }
@@ -10,22 +10,26 @@ function handleError(error) {
 
 function notifyBackgroundPage(e) {
     let sending = browser.runtime.sendMessage({
-        greeting: "Greeting from the content script"
+        focus_mode: true
     });
     sending.then(handleResponse, handleError);
 }
 
+window.addEventListener("load", notifyBackgroundPage);
 
+browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "focus") {
+        //Insert the focus styling
+        YOUTUBE_BROWSING_AREA.textContent = ''; //wipes the browse section on the home page
 
-//Remove the youtube home page area and insert an indicator that the extension is enabled.
-ytBrowse.innerHTML = `<div id="focused-youtube-area">
-    <h1>Productivity Mode enabled.</h1>
-</div>`;
-
-window.addEventListener("load",notifyBackgroundPage);
-
-
-
+        //Remove the youtube home page area and insert an indicator that the extension is enabled.
+        YOUTUBE_BROWSING_AREA.innerHTML = `<div id="focused-youtube-area">
+                                                <h1>Productivity Mode enabled.</h1>
+                                            </div>`;
+    } else if (message.command === "unfocus") {
+        //remove the focus styling
+    }
+});
 
 
 
