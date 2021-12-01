5dd408f319e1bfabc3309d9fdbe15357e743e1fc
Zackyization <zackyization@gmail.com>
2021-12-01 22:53:20 +0800

    Add toggle switch styling to pop up menu

---
+++ popup/focused-youtube-popup.js
@@ -0,0 +1,11 @@
+/* Toggle material toggle span element switch */
+let toggleElements = document.getElementsByClassName("toggle");
+let toggleFunction =  (e)=> {
+    //search siblings for the checkbox input, toggle it upon click
+    let toggleCheckbox = e.originalTarget.parentNode.firstElementChild;
+    toggleCheckbox.checked = !toggleCheckbox.checked;
+}
+
+Array.from(toggleElements).forEach((e) => {
+    e.addEventListener('click', toggleFunction);
+});
