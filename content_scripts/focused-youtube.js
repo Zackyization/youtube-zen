f11c834fe9c0e0302e1fc29604062a582577995a
Zackyization <zackyization@gmail.com>
2021-12-07 16:54:17 +0800

    Make youtube focus message background color aware

--- content_scripts/focused-youtube.js
+++ content_scripts/focused-youtube.js
@@ -9,3 +9,17 @@ YOUTUBE_BROWSING_AREA.innerHTML = `
         </div>
     </section>
 </article>`;
+
+
+
+document.addEventListener("click", (e) => {
+    if (e.target.tagName === "YT-FORMATTED-STRING") {
+        let themeChoice = e.target.innerHTML;
+        let youtubeFocusArea = document.getElementById("focused-youtube-area");
+        if (themeChoice === "Dark theme") {
+            youtubeFocusArea.style.color = "white";
+        } else if (themeChoice === "Light theme") {
+            youtubeFocusArea.style.color = "black";
+        }
+    }
+});
